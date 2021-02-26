466884e0a7cd38fea14ef811497b18d10aac947e
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-26 11:28:57 +0000

    changed var names for menu

--- jasmine-standalone-3.6.0/spec/cartSpec.js
+++ jasmine-standalone-3.6.0/spec/cartSpec.js
@@ -2,9 +2,9 @@ describe('Cart', function() {
   
   var cart;
 
-  var dish = {name: "Kiev", price: 800, displayPrice() {return "£8.00"}};
-  var dish2 = {name: "Four Seasons", price: 800, displayPrice() {return "£8.00"}};
-  var dish3 = {name: "Margherita", price: 650, displayPrice() {return "£6.50"}};
+  var kiev = {name: "Kiev", price: 800, displayPrice() {return "£8.00"}};
+  var fourSeasons = {name: "Four Seasons", price: 800, displayPrice() {return "£8.00"}};
+  var margherita = {name: "Margherita", price: 650, displayPrice() {return "£6.50"}};
 
   beforeEach(function() {
     cart = new Cart();
@@ -15,27 +15,27 @@ describe('Cart', function() {
   });
 
   it('can receive an item', function() {
-    cart.receive(dish);
+    cart.receive(kiev);
     expect(cart.contents[0].name).toEqual("Kiev");
     expect(cart.contents[0].displayPrice()).toEqual("£8.00");
   });
 
   it('totals up the contents of the cart', function() {
-    cart.receive(dish);
-    cart.receive(dish2);
-    cart.receive(dish3);
+    cart.receive(kiev);
+    cart.receive(fourSeasons);
+    cart.receive(margherita);
     expect(cart.displayTotalPrice()).toEqual("£22.50");
   });
 
   it('displays names of dishes and their quantities', function() {
     for (let i = 0; i < 5; i++) {
-      cart.receive(dish);
+      cart.receive(kiev);
     }
 
     for (let i = 0; i < 3; i++) {
-      cart.receive(dish3);
+      cart.receive(margherita);
     }
-    console.log(cart.displayOrder());
+
     expect(cart.displayOrder()).toEqual({Kiev: 5, Margherita: 3, total: "£59.50"});
   });
 
